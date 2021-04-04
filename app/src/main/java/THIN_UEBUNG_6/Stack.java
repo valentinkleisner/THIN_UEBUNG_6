bc5912b8e451d7cbe063368841fa8a1cc9a771bb
kleisval <kleisval@students.zhaw.ch>
2021-04-04 15:22:03 +0200

    CHANGED: implemented Stack

--- app/src/main/java/THIN_UEBUNG_6/Stack.java
+++ app/src/main/java/THIN_UEBUNG_6/Stack.java
@@ -1,15 +1,41 @@
 package main.java.THIN_UEBUNG_6;
 
 public class Stack{
-    String[] stack = new String[99];
-    public final String EOS = "$";
+    public final char EOS = '$';
+    public final Integer STACK_LENGTH = 99;
+    char[] stack = new char[STACK_LENGTH];
 
-    public void pop() {
-
+    public Stack() {
+        stack[0] = EOS;
     }
 
-    public void push() {
+    public void pop() {
+        char[] newStack = new char[STACK_LENGTH];
+        for (int i = 0; i < stack.length; i++) {
+            newStack[i] = stack[i+1];
+            if (stack[i+1] == EOS)
+                break;
+        }
+        stack = newStack;
+    }
 
+    public void push(char character) {
+        char[] newStack = new char[STACK_LENGTH];
+        newStack[0] = character;
+        for (int i = 0; i < stack.length; i++) {
+            newStack[i+1] = stack[i];
+            if (stack[i] == EOS)
+                break;
+        }
+        stack = newStack;
+    }
+
+    public char peek() {
+        return stack[0];
+    }
+
+    public Integer getSize() {
+        return stack.length;
     }
 
 }
