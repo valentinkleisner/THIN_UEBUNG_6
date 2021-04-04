4770a2f55f5a49c8ed9adabd22beab37c5d7fe2d
Michel Hasen <hasmic02@students.zhaw.ch>
2021-04-04 17:10:07 +0200

    removed Test

--- app/src/test/java/THIN_UEBUNG_6/AppTest.java
+++ app/src/test/java/THIN_UEBUNG_6/AppTest.java
@@ -1,12 +1,7 @@
 package THIN_UEBUNG_6;
 
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+
 }
