1e7f21defdf61945c5fc8e277c6e39451d04d585
kleisval <kleisval@students.zhaw.ch>
2021-04-04 13:47:35 +0200

    CHANGED: fixed initial commit

---
+++ app/src/main/java/THIN_UEBUNG_6/Cellar.java
@@ -0,0 +1,4 @@
+package main.java.THIN_UEBUNG_6;
+
+public class Cellar {
+}
---
+++ app/src/main/java/THIN_UEBUNG_6/Stack.java
@@ -0,0 +1,15 @@
+package main.java.THIN_UEBUNG_6;
+
+public class Stack{
+    String[] stack = new String[99];
+    public final String EOS = "$";
+
+    public void pop() {
+
+    }
+
+    public void push() {
+
+    }
+
+}
